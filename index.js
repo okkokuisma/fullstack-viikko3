8f16a1e26bfb9a0558b3bf52b5f2e150bc2bf9d3
Okko Kuisma <okkokuisma@gmail.com>
2021-08-18 13:15:11 +0300

    uibuild

--- index.js
+++ index.js
@@ -24,44 +24,36 @@ app.use(
         ].join(' ')
     }))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
 
-app.get('/api/persons', (request, response) => {
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.get('/api/persons', (request, response, next) => {
     Contact.find({}).then(contacts => {
         response.json(contacts)
     })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
     const date = new Date()
-    response.send(`
-      <p>Phonebook has info of ${persons.length} people</p>
-      <p>${date.toUTCString()}</p>
-    `)
+    Contact.find({}).then(contacts => {
+        response.send(`
+        <p>Phonebook has info of ${contacts.length} people</p>
+        <p>${date.toUTCString()}</p>
+        `)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Contact.findById(request.params.id)
         .then(contact => {
             if (contact) {
@@ -70,41 +62,51 @@ app.get('/api/persons/:id', (request, response) => {
                 response.status(404).end()
             } 
         })
-        .catch(error => {
-            console.log(error)
-            response.status(500).end()
-        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Contact.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end()
-        })  
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    if (body.name === undefined || body.number === undefined) {
-        return response.status(400).json({
-            error: 'no name or number'
-        })
-    }
-    // if (persons.find(person => person.name === body.name)) {
+    // if (body.name === undefined || body.number === undefined) {
     //     return response.status(400).json({
-    //         error: 'name must be unique'
+    //         error: 'no name or number'
     //     })
     // }
 
     const contact = new Contact({
         name: body.name,
-        number: body.number,
+        number: body.number
     })
 
     contact.save().then(savedContact => {
         response.json(savedContact)
     })
+    .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const contact = {
+        name: body.name,
+        number: body.number
+    }
+
+    Contact.findByIdAndUpdate(request.params.id, contact, { new : true })
+        .then(updatedContact => {
+            response.json(updatedContact)
+        })
+        .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- models/contact.js
+++ models/contact.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const contactSchema = mongoose.Schema({
-    name: String,
-    number: String,
+    name: { type: String, required: true, unique: true, minlength: 3 },
+    number: { type: String, required: true, minlength: 8 }
 })
+contactSchema.plugin(uniqueValidator);
 
 contactSchema.set('toJSON', {
     transform: (document, returnedObject) => {
